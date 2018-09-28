74e175a7e97400d8f2f64ac562cccadd9d8af616
Kamesas <semakaleksandr2014@gmail.com>
2018-09-28 15:33:55 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="App">HELLO</div>
+        <div className="App">HELLO</div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;

e09c2b855840a3067639e4e4c4c51672dd3fcd5c
Kamesas <semakaleksandr2014@gmail.com>
2018-09-30 17:03:25 +0300

    added css-modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
+import Layout from "./hoc/layout/Layout";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <div className="App">HELLO</div>
-        <div className="App">HELLO</div>
-      </Fragment>
+      <Layout>
+        <div style={{ width: 400, border: "1px solid #000" }}>
+          <h1>Layout</h1>
+        </div>
+      </Layout>
     );
   }
 }
